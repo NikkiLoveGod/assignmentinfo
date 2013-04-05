3acfd9da5b545f09953f894e1fd880f68911505d
Sami Kurvinen <sami.kurvinen@gmail.com>
2013-04-05 03:06:57 +0000

    Puts up the Unlock table, populates 4 upcoming unlocks with details. Brings a bug to the scrolling detailthing on the right.

--- assignmentinfo.js
+++ assignmentinfo.js
@@ -94,9 +94,37 @@ BBLog.handle("add.plugin", {
     		assignments = instance.getAssignments( instance, name );
 
             console.log(assignments);
+            unfinishedAssignments = instance.getUnfinishedAssignments( instance, assignments );
 
-            sortedAssignments = instance.sortAssignmentsOnCompletion( instance, assignments );
-            console.log(sortedAssignments);
+            /**
+             * Upcoming unlocks HTML copy
+             */
+            $UAtemplate = $('<div id="profile-stats-awards-upcoming" class="profile-stats-type-container"><h1> Upcoming Unlocks </h1><div class="profile-stats-type-container-filters"></div><surf:container id="unlocks"><table cellpadding="0" cellspacing="0" class="leaderboard-venice-specific" id="profile-stats-awards-upcoming-table"><thead><tr><th class="profile-stats-venice-awards-head-completion"> Completion </th><th class="profile-stats-venice-awards-head-award"></th></tr></thead><tbody><tr class="leaderboard-venice-specific-row first filterByKit filterByKit_8"><td class="leaderboard-venice-specific-cell"><div class="profile-stats-venice-awards-body-completion"><div class="profile-stats-venice-awards-body-completion-meter-wrapper"><div class="profile-stats-venice-awards-body-completion-meter" style="width: 99%;"></div></div><span class="profile-stats-venice-awards-body-completion-value"><span class="common-sorting-value"> 99 </span> % </span></div><div class="profile-stats-venice-awards-body-separator"></div></td><td class="leaderboard-venice-specific-cell"><div class="profile-stats-venice-awards-body-award"><table cellspacing="0" cellpadding="0" border="0" class="profile-stats-venice-awards-body-award-padder"><tbody><tr><td class="profile-stats-venice-awards-body-award-class"><div class="common-bf3-kit-icon common-bf3-kit-icon-recon"></div></td><td class="profile-stats-venice-awards-body-award-type-img wide"><a href="/bf3/soldier/N-L-G/iteminfo/m417/392292591/pc/"><img width="90" height="54" src="http://battlelog-cdn.battlefield.com/cdnprefix/eaf9512714a/public/profile/bf3/stats/items_90x54/pkas.png" alt=""></a></td><td class="profile-stats-venice-awards-body-award-info"><div class="profile-stats-venice-awards-body-award-info-title"><span class="common-sorting-value" style="font-size:12px;"><a href="/bf3/soldier/N-L-G/iteminfo/m417/392292591/pc/" style="font-size:12px;"> PKA-S (HOLO) </a></span></div><div class="profile-stats-venice-awards-body-award-info-todo"> 198 / 200 M417 Kills </div></td></tr></tbody></table></div></td></tr></tbody></table></surf:container><div class="base-clear"></div></div>');
+            $UAtable = $UAtemplate.find('#profile-stats-awards-upcoming-table > tbody');
+            $UArow = $UAtemplate.find('.leaderboard-venice-specific-row.first');
+            $UArow.find('.profile-stats-venice-awards-body-award-class').remove();
+            $UArow.remove();
+
+            unfinishedAssignments = unfinishedAssignments.slice(0, 4);
+
+            $.each(unfinishedAssignments, function(k, assignment) {
+                var $UArowClone = $UArow.clone();
+                $UArowClone.find('.profile-stats-venice-awards-body-completion-value > .common-sorting-value').text(assignment.completion * 100);
+                $UArowClone.find('.profile-stats-venice-awards-body-award-info-title a').attr('href', '#'+assignment.id).text( assignment.name + ' (' + assignment.desc + ')');
+
+                var criteriaText = '';
+                $.each(assignment.criteria, function(k, criteria) {
+                    if ( criteria.completion < 1 ) {
+                        criteriaText += '('+criteria.curr +' / ' + criteria.needed + ') ' + criteria.nname + '<br />';
+                    }
+                });
+
+                $UArowClone.find('.profile-stats-venice-awards-body-award-type-img').html('<img src="http://files.bf3stats.com/img/bf3/' + assignment.img + '" style="width: 130px" alt="' + assignment.desc + '" />');
+                $UArowClone.find('.profile-stats-venice-awards-body-award-info-todo').html( criteriaText );
+                $UAtable.append($UArowClone);
+            });
+            
+            $assignmentsContainer.find('#assignments-1024').before($UAtemplate);
 
             /**
              * Go through the assignments and put up the header dom
@@ -147,9 +175,9 @@ BBLog.handle("add.plugin", {
 			data: { 
 				player: name, 
 				opt: {
-                    clear: true,
+                    all: true,
 					assignments: true,
-                    assignmentsName: true                    
+                    assignmentsName: true
 				}
 			},
 			dataType: 'json'
@@ -159,12 +187,15 @@ BBLog.handle("add.plugin", {
 				return false;
 			}
 
+            console.log(data);
+
 			/**
 			 * Unwrap the hierarchy from assignments and store them as assignmentId : assignmentData type object in "assignments" var
              * Also put up the assignment completion % in the object as well.
  			 */
 			$.each(data.stats.assignments, function( groupId, assignmentGroup ) {
 				$.each(assignmentGroup, function( assignmentId, assignment ) {
+                    assignment.id = assignmentId;
                     assignment = instance.getAssignmentCompletion( instance, assignment );
 					assignments[assignmentId] = assignment;
 				});
@@ -190,6 +221,24 @@ BBLog.handle("add.plugin", {
         assignment.completion = Math.round(sum / assignment.criteria.length * 100) / 100;
         return assignment;
     },
+
+
+    /**
+     * Filters out any assignments that are completed and orders them by completion
+     */
+    getUnfinishedAssignments : function ( instance, assignments ) {
+        var unfinishedAssignments = [];
+        var sortedAssignments = instance.sortAssignmentsOnCompletion( instance, assignments );
+
+        $.each( sortedAssignments, function(k, assignment ) {
+            if ( assignment.completion < 1 ) {
+                unfinishedAssignments[unfinishedAssignments.length] = assignment;
+            }
+        });
+
+        return unfinishedAssignments;
+    },
+
 
     /**
      * Sorts  the given assignment object according to completion
